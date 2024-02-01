a127a57d0f51f37bc116ea6edb8be365660abb7a
Elías <elias.ludviksson@gmail.com>
2024-02-01 18:33:26 +0000

    parse of mikid of litid?

--- src/generate.js
+++ src/generate.js
@@ -1,20 +1,18 @@
-import { parse } from 'path';
 import {
 	createDirIfNotExists,
-	readFile,
 	readFilesFromDir,
-	writeFile,
+	writeFile
 } from './lib/file.js';
 import {
 	hlekkur,
 	htmlListString,
-	returnMatchdiv,
 	tabletemplate,
 	teksti,
 	template
 } from './lib/html.js';
-import { stig } from './lib/score.js';
-import { nameScoreValidation } from './lib/validation.js';
+import {
+	parseTeamsJson
+} from './lib/parse.js';
 
 const INPUT_DIR = './data';
 const OUTPUT_DIR = './dist';
@@ -23,54 +21,9 @@ async function main() {
 	await createDirIfNotExists(OUTPUT_DIR);
 	const files = await readFilesFromDir(INPUT_DIR);
 	const teamsFile = files.find(file => file.includes('teams'));
-	let teams = (teamsFile ? await readFile(teamsFile) : '')
-	teams = teams ? JSON.parse(teams) : [];
-	const stada = {};
-	if (!teams) {
-		throw new Error('tókst ekki að finna lið í teams.json')
-	} else {
-		for (const stak of teams) {
-			stada[stak] = 0;
-		}
-	}
-	const leikir = []
-	for await (const file of files) {
-		if (!parse(file)?.name.includes('gameday')) {
-			continue;
-		}
-		const fileContents = await readFile(file);
-		const fcJson = fileContents && JSON.parse(fileContents)
-		if (!fcJson?.date ||
-			!fcJson?.games ||
-			fcJson.date !== (new Date(fcJson.date)).toISOString()) // athugar hvort dagsetning séu iso
-		{
-			continue
-		}
-		const leikur = []
-		for (const { home, away } of Array.from(fcJson.games)) {
-			const heima = nameScoreValidation(home, teams);
-			const uti = nameScoreValidation(away, teams);
-			if (heima && uti) {
-				const vann = stig(heima.score, uti.score)
-				stada[heima.name] += vann[0];
-				stada[uti.name] += vann[1];
-				leikur.push(htmlListString('leikur__lid', false, // búum til lista fyrir hvern leik
-					returnMatchdiv(true, heima.name, heima.score, vann[0]),
-					returnMatchdiv(false, uti.name, uti.score, vann[1]))
-				)
-				stada[heima.name] += vann[0];
-				stada[uti.name] += vann[1];
-			}
-		}
-		leikir.push({
-			date: new Date(fcJson.date),
-			html: `<p class="leikir__dagsetning">${fcJson.date}</p>${htmlListString('leikir__leikirnir', false, leikur)}`
-		})
-	}
+	const { leikir, tbody } = await parseTeamsJson(teamsFile, files)
 	leikir.sort((a, b) => a.date - b.date)
 	const sortleikir = leikir.map(stak => stak.html)
-	const tbody = teams.map(stak =>
-		[stak, stada[stak]])
 	const nav = `<nav>${htmlListString('hlekkir', false,
 		hlekkur('/', 'forsíða'),
 		hlekkur('/leikir.html', 'leikir'),
@@ -95,7 +48,6 @@ async function main() {
 	} catch (e) {
 		console.error(e)
 	}
-
 }
 
 main().catch((error) => {
--- src/lib/parse.js
+++ src/lib/parse.js
@@ -1,4 +1,55 @@
-// to do gera eh hérna
-export function parseTeamsJson(data) {
-	return JSON.parse(data);
+import { parse } from 'path';
+import {
+	readFile
+} from './file.js';
+import {
+	htmlListString,
+	returnMatchdiv
+} from './html.js';
+import { stig } from './score.js';
+import { nameScoreValidation } from './validation.js';
+
+export async function parseTeamsJson(data, files) {
+	const stada = {};
+	const leikir = [];
+	const teams = JSON.parse(data);
+	for (const stak of teams) {
+		stada[stak] = 0;
+	}
+	for await (const file of files) {
+		if (!parse(file)?.name.includes('gameday')) {
+			continue;
+		}
+		const fileContents = await readFile(file);
+		const fcJson = fileContents && JSON.parse(fileContents)
+		if (!fcJson?.date ||
+			!fcJson?.games ||
+			fcJson.date !== (new Date(fcJson.date)).toISOString()) // athugar hvort dagsetning séu iso
+		{
+			continue
+		}
+		const leikur = []
+		for (const { home, away } of Array.from(fcJson.games)) {
+			const heima = nameScoreValidation(home, teams);
+			const uti = nameScoreValidation(away, teams);
+			if (heima && uti) {
+				const vann = stig(heima.score, uti.score)
+				stada[heima.name] += vann[0];
+				stada[uti.name] += vann[1];
+				leikur.push(htmlListString('leikur__lid', false, // búum til lista fyrir hvern leik
+					returnMatchdiv(true, heima.name, heima.score, vann[0]),
+					returnMatchdiv(false, uti.name, uti.score, vann[1]))
+				)
+				stada[heima.name] += vann[0];
+				stada[uti.name] += vann[1];
+			}
+		}
+		leikir.push({
+			date: new Date(fcJson.date),
+			html: `<p class="leikir__dagsetning">${fcJson.date}</p>${htmlListString('leikir__leikirnir', false, leikur)}`
+		})
+	}
+	const tbody = teams.map(stak =>
+		[stak, stada[stak]])
+	return { leikir, tbody };
 }
