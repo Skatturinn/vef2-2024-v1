678398230b9f2223268856c91b9de9ed36fd56d0
Elías <elias.ludviksson@gmail.com>
2024-02-02 04:49:10 +0000

    toEqual? or not too Equal

--- src/lib/file.test.js
+++ src/lib/file.test.js
@@ -35,7 +35,7 @@ describe('file', () => {
 		it('should return array of known files for dir that does exist', async () => {
 			const result = await readFilesFromDir(testDir);
 
-			expect(result).toBe([
+			expect(result).toEqual([
 				'src/test/data/1',
 				'src/test/data/2',
 				'src/test/data/test',
